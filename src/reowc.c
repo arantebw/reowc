b5cd51353cd11bb17c84c61e62290cc841db9caf
Billy Arante <arantebillywilson@gmail.com>
2025-03-30 20:56:43 +0800

    feat(src): ✨ check if target file and an option is provided

--- src/reowc.c
+++ src/reowc.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() {
-  printf("Hello, world!\n");
+int main(int argc, char *argv[]) {
+  if (argc > 1 == 0) {
+    printf("Error: Provide a target file and at least of the following "
+           "options:\n");
+    printf("-c  return the file size in bytes\n\n");
+    printf("Help:\n");
+    printf("reowc [option] [target_file]\n");
+
+    return 1;
+  }
+
   return 0;
 }
