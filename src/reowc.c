23ede1d8f3eefc50e80feba5f0d032135c973e15
Billy Arante <arantebillywilson@gmail.com>
2025-04-01 21:40:51 +0800

    feat(reowc.c): add option on help text

--- src/reowc.c
+++ src/reowc.c
@@ -6,7 +6,8 @@ int main(int argc, char *argv[]) {
   if (argc > 1 == 0) {
     printf("Error: Provide a target file and at least of the following "
            "options:\n");
-    printf("-c  return the file size in bytes\n\n");
+    printf("-c  file size (bytes)\n");
+    printf("-l  number of lines\n\n");
     printf("Help:\n");
     printf("reowc [option] [target_file]\n");
 
