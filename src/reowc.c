969abc08add1df3ae35d423ba9b73d9e0361ca04
Billy Arante <arantebillywilson@gmail.com>
2025-04-02 20:44:36 +0800

    refactor: ♻️ create the get_lines_count function

--- src/reowc.c
+++ src/reowc.c
@@ -13,6 +13,20 @@ void get_file_size(char *file) {
   printf("%ld\t%s\n", file_size, file);
 }
 
+void get_lines_count(char *file) {
+  FILE *input_file = fopen(file, "r");
+  int c;
+  int count = 0;
+  while ((c = fgetc(input_file)) != EOF) {
+    if (c == '\n') {
+      count++;
+    }
+  }
+  fclose(input_file);
+
+  printf("%d\t%s\n", count, file);
+}
+
 int main(int argc, char *argv[]) {
   // Checks the input arguments
   if (argc > 1 == 0) {
@@ -26,20 +40,8 @@ int main(int argc, char *argv[]) {
     return 1;
   } else if (strcmp(argv[1], "-c") == 0) {
     get_file_size(argv[2]);
-  }
-  // Return the number of lines
-  else if (strcmp(argv[1], "-l") == 0) {
-    FILE *input_file = fopen(argv[2], "r");
-    int ch;
-    int line_count = 0;
-    while ((ch = fgetc(input_file)) != EOF) {
-      if (ch == '\n') {
-        line_count++;
-      }
-    }
-    fclose(input_file);
-
-    printf("%d\t%s\n", line_count, argv[2]);
+  } else if (strcmp(argv[1], "-l") == 0) {
+    get_lines_count(argv[2]);
   }
   // Return the number of words
   else if (strcmp(argv[1], "-w") == 0) {
