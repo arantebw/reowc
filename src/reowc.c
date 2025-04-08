fcb7786003bf07b236920a1283cc515b16df94e7
Billy Arante <arantebillywilson@gmail.com>
2025-04-08 23:22:39 +0800

    refactor(src/reowc.c): move the get_file_size function to a file

--- src/reowc.c
+++ src/reowc.c
@@ -4,23 +4,7 @@
 #include <string.h>
 #include <wchar.h>
 
-long get_file_size(char *file) {
-  long file_size = 0;
-
-  if (file != NULL) {
-    FILE *input_file = fopen(file, "rb");
-    fseek(input_file, 0, SEEK_END);
-    file_size = ftell(input_file);
-    fclose(input_file);
-  } else {
-    int ch;
-    while ((ch = fgetc(stdin)) != EOF) {
-      file_size++;
-    }
-  }
-
-  return file_size;
-}
+#include "get_file_size.h"
 
 int get_lines_count(char *file) {
   int c;
