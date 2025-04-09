2a7a879bbabb9a7cd4ea9808a8113d8aadc63ef9
Billy Arante <arantebillywilson@gmail.com>
2025-04-09 23:47:44 +0800

    feat(src/reowc.c): move the function

--- src/reowc.c
+++ src/reowc.c
@@ -1,46 +1,10 @@
 #include "get_file_size.h"
 #include "get_lines_count.h"
-
-#include <ctype.h>
+#include "get_words_count.h"
 #include <locale.h>
 #include <stdio.h>
 #include <string.h>
 #include <wchar.h>
-
-int get_words_count(char *file) {
-  int c;
-  int count = 0;
-  int in_word = 0;
-
-  if (file != NULL) {
-    FILE *input_file = fopen(file, "r");
-    // Traverse each character
-    while ((c = fgetc(input_file)) != EOF) {
-      if (isspace(c)) {
-        if (in_word) { // a word is found
-          count++;
-          in_word = 0;
-        }
-      } else {
-        in_word = 1;
-      }
-    }
-    fclose(input_file); // close a file stream
-  } else {
-    while ((c = fgetc(stdin)) != EOF) {
-      if (isspace(c)) {
-        if (in_word) { // a word is found
-          count++;
-          in_word = 0;
-        }
-      } else {
-        in_word = 1;
-      }
-    }
-  }
-
-  return count;
-}
 
 int get_characters_count(char *file) {
   setlocale(LC_ALL, ""); // set locale to default environment locale
