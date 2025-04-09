e79a421a51b43f1a4d9195da2d8e48cc9ac36edf
Billy Arante <arantebillywilson@gmail.com>
2025-04-09 23:32:51 +0800

    feat(src/reowc.c): move the function

--- src/reowc.c
+++ src/reowc.c
@@ -1,32 +1,11 @@
 #include "get_file_size.h"
+#include "get_lines_count.h"
+
 #include <ctype.h>
 #include <locale.h>
 #include <stdio.h>
 #include <string.h>
 #include <wchar.h>
-
-int get_lines_count(char *file) {
-  int c;
-  int count = 0;
-
-  if (file != NULL) {
-    FILE *input_file = fopen(file, "r");
-    while ((c = fgetc(input_file)) != EOF) {
-      if (c == '\n') {
-        count++;
-      }
-    }
-    fclose(input_file);
-  } else {
-    while ((c = fgetc(stdin)) != EOF) {
-      if (c == '\n') {
-        count++;
-      }
-    }
-  }
-
-  return count;
-}
 
 int get_words_count(char *file) {
   int c;
